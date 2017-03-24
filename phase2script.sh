f0579d08c80438ec95938b9b2827fc773047f899
kvongaza <kiefer@vongaza.ca>
2017-03-23 22:27:36 -0600

    additional comments
    -pipe to null instead of -f
    -can use db_dump -p tw.idx to check if valid.

--- phase2script.sh
+++ phase2script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #remove tweets.idx, dates.idx and terms.idx; otherwise we just append
-rm -f tw.idx da.idx te.idx
+rm tw.idx da.idx te.idx > /dev/null #pipe to null
 # sort/read      per line    replace blah: with blah\n  replace : with something that looks like :
 #                                                                       load with db_load    output to tw.idx
 sort -u tweets.txt | perl -lpe 's/^([^:]*):/$1\n/g;' -lpe 's/\\/&92;/g' | db_load -T -t hash tw.idx
