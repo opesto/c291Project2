082a77762da0f6e3393ba17deb74f0553ccea911
Kiefer Von Gaza <kvongaza@cs.ualberta.ca>
2017-04-02 19:30:46 -0600

    fixed phase2 for duplicate key, unique values

--- phase2script.sh
+++ phase2script.sh
@@ -4,6 +4,6 @@
 rm tw.idx da.idx te.idx > /dev/null #pipe to null
 # sort/read      per line    replace blah: with blah\n  replace : with something that looks like :
 #                                                                       load with db_load    output to tw.idx
-sort -u tweets.txt | perl -lpe 's/^([^:]*):/$1\n/g;' -lpe 's/\\/&92;/g' | db_load -T -t hash tw.idx
-sort -u dates.txt | perl -lpe 's/^([^:]*):/$1\n/g;' -lpe 's/\\/&92;/g' | db_load -T -t btree da.idx
-sort -u terms.txt | perl -lpe 's/^([^:]*):/$1\n/g;' -lpe 's/\\/&92;/g' | db_load -T -t btree te.idx
+sort -u tweets.txt | perl -lpe 's/^([^:]*):/$1\n/g;' -lpe 's/\\/&92;/g' | db_load -c duplicates=1 -T -t hash tw.idx
+sort -u dates.txt | perl -lpe 's/^([^:]*):/$1\n/g;' -lpe 's/\\/&92;/g' | db_load -c duplicates=1 -T -t btree da.idx
+sort -u terms.txt | perl -lpe 's/^([^:]*):/$1\n/g;' -lpe 's/\\/&92;/g' | db_load -c duplicates=1 -T -t btree te.idx
